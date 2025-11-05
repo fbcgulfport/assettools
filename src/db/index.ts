d9576482944c4c1214be1cfcf41fc841fd5dcd7c
Will Shadow <wshadow@fbcgulfport.org>
2025-11-05 11:16:04 -0600

    migrations

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,10 @@
 import { Database } from "bun:sqlite"
 import { drizzle } from "drizzle-orm/bun-sqlite"
+import { migrate } from "drizzle-orm/bun-sqlite/migrator"
 import * as schema from "./schema"
 
 const sqlite = new Database("assettools.db")
 export const db = drizzle(sqlite, { schema })
+await migrate(db)
 
 export * from "./schema"
